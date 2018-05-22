d12b6dd5e472bff978b8fa5a63aa33f8e96692aa
Jennifer Yang <yyang@lcs-mba3025.lcs.on.ca>
2018-05-22 15:16:01 -0400

    Fixed everything.

--- SnowCalls/main.swift
+++ SnowCalls/main.swift
@@ -35,56 +35,63 @@ while 1 == 1 {
 
 // Global variable to track how many phone numbers we can expect from the user
 
-    
+
 // Add code below to ask the user for how many phones numbers will actually be provided
 
-    
+
 
 // PROCESS & OUTPUT
 // Implement the primary logic of the problem here
 
 // Collect the phone numbers
+var translatedPhoneNumber = ""
 
-        
-    // Create the translated phone number
-    var translatedPhoneNumber = ""
-    
-    for n in 1...expectedLines {
+
+// Create the translated phone number
+
+for n in 1...expectedLines {
     // Prompt the user
     print("Enter phone number \(n):")
     
     // Get the input (use guard-let to guarantee it is not nil)
     // and then print it out
-    guard readLine() != nil else {
+    guard let integerInput = readLine() else {
         // If someone enters nil input, just skip to the next line
         continue
     }
     
     // Replace this logic with the correct logic
-    switch String(n) {
-    case "A", "B", "C":
-    translatedPhoneNumber = "2"
-    case "D", "E", "F":
-    translatedPhoneNumber = "3"
-    case "G", "H", "I":
-    translatedPhoneNumber = "4"
-    case "J", "K", "L":
-    translatedPhoneNumber = "5"
-    case "M", "N", "O":
-    translatedPhoneNumber = "6"
-    case "P", "Q", "R", "S":
-    translatedPhoneNumber = "7"
-    case "T", "U", "V":
-    translatedPhoneNumber = "8"
-    case "W", "X", "Y", "Z":
-    translatedPhoneNumber = "9"
-    default :
-        translatedPhoneNumber += String(n)
+    // loop through each character in input
+    for character in integerInput {
+        switch character {
+        case "A", "B", "C":
+            translatedPhoneNumber += "2"
+        case "D", "E", "F":
+            translatedPhoneNumber += "3"
+        case "G", "H", "I":
+            translatedPhoneNumber += "4"
+        case "J", "K", "L":
+            translatedPhoneNumber += "5"
+        case "M", "N", "O":
+            translatedPhoneNumber += "6"
+        case "P", "Q", "R", "S":
+            translatedPhoneNumber += "7"
+        case "T", "U", "V":
+            translatedPhoneNumber += "8"
+        case "W", "X", "Y", "Z":
+            translatedPhoneNumber += "9"
+        default :
+            translatedPhoneNumber += String(character)
+            
+        }
+        
     }
     // Print out the converted number
     print("This is \(translatedPhoneNumber).")
-    
-
 }
+
+
+
+
 
 
